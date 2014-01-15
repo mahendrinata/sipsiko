4e8145499cf66566523bbf6f4ad27c4cb0bc7f6e
MAHE <MAHE@MAHE-PC>
2014-01-15 23:15:59 +0700

    ADD - Company Model

---
+++ application/models/company.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!defined('BASEPATH'))
+  exit('No direct script access allowed');
+
+/**
+ * User model use to add all behavior user
+ * @author Mahendri Winata <mahen.0112@gmail.com>
+ */
+class Company extends App_Model {
+
+  public $fields = array(
+      array('name' => 'id', 'type' => 'integer', 'require' => TRUE, 'primary_key' => TRUE, 'auto_increment' => TRUE),
+      array('name' => 'slug', 'type' => 'varchar', 'require' => TRUE, 'unique' => TRUE, 'index' => TRUE),
+      array('name' => 'name', 'type' => 'varchar', 'require' => TRUE),
+      array('name' => 'description', 'type' => 'text'),
+      array('name' => 'status', 'type' => 'varchar'),
+      array('name' => 'user_id', 'type' => 'integer', 'index' => TRUE),
+      array('name' => 'created_at', 'type' => 'datetime'),
+      array('name' => 'updated_at', 'type' => 'datetime'),
+  );
+  
+}
+
+?>
