5742b82460bcab5de00773d4e103d5e5457b1f80
MAHE <MAHE@MAHE-PC>
2014-01-15 23:17:29 +0700

    ADD - Employee Model

---
+++ application/models/employee.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!defined('BASEPATH'))
+  exit('No direct script access allowed');
+
+/**
+ * User model use to add all behavior user
+ * @author Mahendri Winata <mahen.0112@gmail.com>
+ */
+class Employee extends App_Model {
+
+  public $fields = array(
+      array('name' => 'id', 'type' => 'integer', 'require' => TRUE, 'primary_key' => TRUE, 'auto_increment' => TRUE),
+      array('name' => 'status', 'type' => 'varchar'),
+      array('name' => 'user_id', 'type' => 'integer', 'index' => TRUE),
+      array('name' => 'company_id', 'type' => 'integer', 'index' => TRUE),
+      array('name' => 'created_at', 'type' => 'datetime'),
+      array('name' => 'updated_at', 'type' => 'datetime'),
+  );
+  
+}
+
+?>
