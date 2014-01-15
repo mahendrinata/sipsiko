ab6443cb17fc2ab4376068c4aaaff50dced86ea2
MAHE <MAHE@MAHE-PC>
2014-01-15 23:28:50 +0700

    ADD - UserTestAnswer Model

---
+++ application/models/user_test_answer.php
@@ -0,0 +1,22 @@
+<?php
+
+if (!defined('BASEPATH'))
+  exit('No direct script access allowed');
+
+/**
+ * User model use to add all behavior user
+ * @author Mahendri Winata <mahen.0112@gmail.com>
+ */
+class UserTestAnswer extends App_Model {
+
+  public $fields = array(
+      array('name' => 'id', 'type' => 'integer', 'require' => TRUE, 'primary_key' => TRUE, 'auto_increment' => TRUE),
+      array('name' => 'user_test_id', 'type' => 'integer', 'index' => TRUE),
+      array('name' => 'answer_id', 'type' => 'integer', 'index' => TRUE),
+      array('name' => 'created_at', 'type' => 'datetime'),
+      array('name' => 'updated_at', 'type' => 'datetime'),
+  );
+  
+}
+
+?>
