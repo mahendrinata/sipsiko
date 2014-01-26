5c89e92fde60f875c647463683320e98c4aeb1e2
MAHE <MAHE@MAHE-PC>
2014-01-26 18:25:45 +0700

    enum class

---
+++ application/models/Status.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!defined('BASEPATH'))
+  exit('No direct script access allowed');
+
+abstract class Status extends BasicEnum {
+
+  const ACTIVE = 'ACTIVE';
+  const INACTIVE = 'INACTIVE';
+  const VOID = 'VOID';
+
+  public static function get_list() {
+    return array(
+      Status::ACTIVE,
+      Status::INACTIVE,
+      Status::VOID
+    );
+  }
+
+  public static function get_map() {
+    $map = array();
+    foreach (Status::get_list() as $status) {
+      $map[$status] = $status;
+    }
+    return $map;
+  }
+
+}
+
+?>
