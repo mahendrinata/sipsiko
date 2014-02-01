55c6cf4c5d79a4d24f81c3ea9022271b0f2b6cb3
Mahendri Winata <mahen.0112@gmail.com>
2014-02-01 11:17:10 +0700

    add new models

---
+++ application/models/answer.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!defined('BASEPATH'))
+  exit('No direct script access allowed');
+
+/**
+ * Answer model use to add all behavior Answer
+ * @author Mahendri Winata <mahen.0112@gmail.com>
+ */
+class Answer extends App_Model {
+
+  public $fields = array(
+    array('name' => 'id', 'type' => 'integer', 'require' => TRUE, 'primary_key' => TRUE, 'auto_increment' => TRUE),
+    array('name' => 'description', 'type' => 'text'),
+    array('name' => 'value', 'type' => 'integer'),
+    array('name' => 'question_id', 'type' => 'integer', 'index' => TRUE),
+    array('name' => 'created_at', 'type' => 'datetime'),
+    array('name' => 'updated_at', 'type' => 'datetime'),
+  );
+
+}
+
+?>
---
+++ application/models/user_test.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!defined('BASEPATH'))
+  exit('No direct script access allowed');
+
+/**
+ * UserTest model use to add all behavior UserTest
+ * @author Mahendri Winata <mahen.0112@gmail.com>
+ */
+class User_test extends App_Model {
+
+  public $fields = array(
+    array('name' => 'id', 'type' => 'integer', 'require' => TRUE, 'primary_key' => TRUE, 'auto_increment' => TRUE),
+    array('name' => 'time_spent', 'type' => 'integer'),
+    array('name' => 'status', 'type' => 'varchar'),
+    array('name' => 'user_id', 'type' => 'integer', 'index' => TRUE),
+    array('name' => 'test_id', 'type' => 'integer', 'index' => TRUE),
+    array('name' => 'created_at', 'type' => 'datetime'),
+    array('name' => 'updated_at', 'type' => 'datetime'),
+  );
+
+}
+
+?>
